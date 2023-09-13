b391309349f1204d9983fdd3f3725b786c731b17
SeniorCoder007 <SeniorCoder007>
2023-09-13 18:48:10 +0100

    backup before messing up

--- Gestion-main/gestionStock/settings.py
+++ Gestion-main/gestionStock/settings.py
@@ -97,8 +97,12 @@ SIMPLE_JWT = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Application',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
