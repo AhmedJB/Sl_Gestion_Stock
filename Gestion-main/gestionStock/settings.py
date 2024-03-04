ec21db190cb4c1a90326658d5f193039fd594832
Ahmed Jabnati <vintex@Ahmeds-MacBook-Pro.local>
2024-03-04 20:14:12 +0100

    deleted workenv

--- Gestion-main/gestionStock/settings.py
+++ Gestion-main/gestionStock/settings.py
@@ -110,8 +110,12 @@ CORS_ALLOWED_ORIGINS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Application',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'database',
+        'PORT': '5432',
     }
 }
 
