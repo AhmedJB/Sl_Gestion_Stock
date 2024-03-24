07f7791e0df5e5b57fa66452588ad66700cd04ae
skyrred <sky.red2212@gmail.com>
2024-03-24 22:53:09 +0000

    update config

--- Gestion-main/gestionStock/settings.py
+++ Gestion-main/gestionStock/settings.py
@@ -110,12 +110,8 @@ CORS_ALLOWED_ORIGINS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Application',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'database',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
